81ef22eb85aa5552c29cdfbecef081bcf7f7bc24
pongdang <pongdang0121@gmail.com>
2021-09-14 01:51:34 +0900

    리팩토링햇슴니다!

--- index.js
+++ index.js
@@ -1,59 +1,25 @@
-let quickMenuBtn1 = document.querySelector("#quick-subsidiary img");
-let quickMenuBtn2 = document.querySelector("#quick-story img");
-let quickMenuBtn3 = document.querySelector("#quick-news img");
-let quickMenuBtn4 = document.querySelector("#quick-location img");
+const quickMenuList = document.querySelectorAll(".quick-menu li");
+const changeImageSrc = (event, extension) => {
+  const imageName = event.target.dataset["menu"];
+  const img = event.target.querySelector("img");
+  img.src = `/image/quick-menu/${imageName}.${extension}`;
+};
 
-quickMenuBtn1.addEventListener("mouseover", startHover1);
-quickMenuBtn1.addEventListener("mouseout", endHover1);
-quickMenuBtn2.addEventListener("mouseover", startHover2);
-quickMenuBtn2.addEventListener("mouseout", endHover2);
-quickMenuBtn3.addEventListener("mouseover", startHover3);
-quickMenuBtn3.addEventListener("mouseout", endHover3);
-quickMenuBtn4.addEventListener("mouseover", startHover4);
-quickMenuBtn4.addEventListener("mouseout", endHover4);
-
-function startHover1() {
-  quickMenuBtn1.src = "/image/quick-menu/icon-subsidiary.gif";
-}
-function startHover2() {
-  quickMenuBtn2.src = "/image/quick-menu/icon-story.gif";
-}
-function startHover3() {
-  quickMenuBtn3.src = "/image/quick-menu/icon-paper.gif";
-}
-function startHover4() {
-  quickMenuBtn4.src = "/image/quick-menu/icon-map.gif";
-}
-function endHover1() {
-  quickMenuBtn1.src = "/image/quick-menu/subsidiary-off.png";
-}
-function endHover2() {
-  quickMenuBtn2.src = "/image/quick-menu/story-off.png";
-}
-function endHover3() {
-  quickMenuBtn3.src = "/image/quick-menu/paper-off.png";
-}
-function endHover4() {
-  quickMenuBtn4.src = "/image/quick-menu/map-off.png";
+for (let i = 0; i < quickMenuList.length; i++) {
+  const quickMenu = quickMenuList[i];
+  quickMenu.addEventListener("mouseenter", (event) =>
+    changeImageSrc(event, "gif")
+  );
+  quickMenu.addEventListener("mouseenter", (event) =>
+    changeImageSrc(event, "png")
+  );
 }
 
-let slideBtn1 = document.querySelector(".carousel-btn-first");
-let slideBtn2 = document.querySelector(".carousel-btn-second");
-let slideBtn3 = document.querySelector(".carousel-btn-third");
-
-let slideImg1 = document.querySelector("#carousel-box1 img");
-let slideImg2 = document.querySelector("#carousel-box2 img");
-let slideImg3 = document.querySelector("#carousel-box3 img");
-
-window.addEventListener("click", function (event) {
-  console.log(event);
-  if (event.target === slideBtn1) {
-    slideImg1.src = "/image/slide/slide-img-1.jpeg";
-  }
-  if (event.target === slideBtn2) {
-    slideImg1.src = "/image/slide/slide-img-2.jpeg";
-  }
-  if (event.target === slideBtn3) {
-    slideImg1.src = "/image/slide/slide-img-3.jpeg";
-  }
+const carousel = document.querySelector(".main-carousel-container");
+carousel.addEventListener("click", (event) => {
+  const slideImg = document.querySelector("#carousel-box1 img");
+  slideImg.src = `/image/slide/slide-img-${event.target.dataset.index}.jpeg`;
 });
+
+// function () {}
+// () => {}
