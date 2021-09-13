e660af9dcda4310bd59abb41ff9119dd939a3425
pongdang <pongdang0121@gmail.com>
2021-09-13 19:51:33 +0900

    슬라이드의 버튼을 누르면 그 버튼에 해당하는 사진으로 이동

--- index.js
+++ index.js
@@ -36,3 +36,24 @@ function endHover3() {
 function endHover4() {
   quickMenuBtn4.src = "/image/quick-menu/map-off.png";
 }
+
+let slideBtn1 = document.querySelector(".carousel-btn-first");
+let slideBtn2 = document.querySelector(".carousel-btn-second");
+let slideBtn3 = document.querySelector(".carousel-btn-third");
+
+let slideImg1 = document.querySelector("#carousel-box1 img");
+let slideImg2 = document.querySelector("#carousel-box2 img");
+let slideImg3 = document.querySelector("#carousel-box3 img");
+
+window.addEventListener("click", function (event) {
+  console.log(event);
+  if (event.target === slideBtn1) {
+    slideImg1.src = "/image/slide/slide-img-1.jpeg";
+  }
+  if (event.target === slideBtn2) {
+    slideImg1.src = "/image/slide/slide-img-2.jpeg";
+  }
+  if (event.target === slideBtn3) {
+    slideImg1.src = "/image/slide/slide-img-3.jpeg";
+  }
+});
