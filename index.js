d03f47e9c9d1d65f9e75a9ce05bc701e96b4b3a4
pongdang <pongdang0121@gmail.com>
2021-09-14 01:55:36 +0900

    찐 리팩토링

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const changeImageSrc = (event, extension) => {
   const imageName = event.target.dataset["menu"];
   const img = event.target.querySelector("img");
   img.src = `/image/quick-menu/${imageName}.${extension}`;
+  console.log(imageName, img);
 };
 
 for (let i = 0; i < quickMenuList.length; i++) {
@@ -10,15 +11,17 @@ for (let i = 0; i < quickMenuList.length; i++) {
   quickMenu.addEventListener("mouseenter", (event) =>
     changeImageSrc(event, "gif")
   );
-  quickMenu.addEventListener("mouseenter", (event) =>
+  quickMenu.addEventListener("mouseleave", (event) =>
     changeImageSrc(event, "png")
   );
 }
 
 const carousel = document.querySelector(".main-carousel-container");
 carousel.addEventListener("click", (event) => {
-  const slideImg = document.querySelector("#carousel-box1 img");
-  slideImg.src = `/image/slide/slide-img-${event.target.dataset.index}.jpeg`;
+  if (event.target.nodeName === "BUTTON") {
+    const slideImg = document.querySelector("#carousel-box1 img");
+    slideImg.src = `/image/slide/slide-img-${event.target.dataset.index}.jpeg`;
+  }
 });
 
 // function () {}
