33fbf0d016e3fd11bd6996f44f723604e5996564
pongdang <pongdang0121@gmail.com>
2021-09-14 12:06:04 +0900

    얼레벌레 이미지 슬라이드

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// 퀵메뉴
 const quickMenuList = document.querySelectorAll(".quick-menu li");
 const changeImageSrc = (event, extension) => {
   const imageName = event.target.dataset["menu"];
   const img = event.target.querySelector("img");
   img.src = `/image/quick-menu/${imageName}.${extension}`;
-  console.log(imageName, img);
+  // console.log(imageName, img);
 };
 
 for (let i = 0; i < quickMenuList.length; i++) {
@@ -16,13 +17,35 @@ for (let i = 0; i < quickMenuList.length; i++) {
   );
 }
 
-const carousel = document.querySelector(".main-carousel-container");
-carousel.addEventListener("click", (event) => {
+// function () {}
+// () => {}
+
+const ACTIVE_CLASS = "active";
+const firstCarousel = document.querySelector(".carousel-item:first-child");
+firstCarousel.classList.add(ACTIVE_CLASS);
+function carousel() {
+  const currentCarousel = document.querySelector(`.${ACTIVE_CLASS}`);
+  if (currentCarousel) {
+    currentCarousel.classList.remove(ACTIVE_CLASS);
+    const nextCarousel = currentCarousel.nextElementSibling;
+    if (nextCarousel) {
+      nextCarousel.classList.add(ACTIVE_CLASS);
+    } else {
+      firstCarousel.classList.add(ACTIVE_CLASS);
+    }
+  } else {
+    firstCarousel.classList.add(ACTIVE_CLASS);
+  }
+}
+carousel();
+setInterval(carousel, 3000);
+
+// 이미지 버튼을 누르면 해당하는 이미지가 나타남
+const carouselBtn = document.querySelector(".main-carousel-container");
+carouselBtn.addEventListener("click", (event) => {
   if (event.target.nodeName === "BUTTON") {
     const slideImg = document.querySelector("#carousel-box1 img");
     slideImg.src = `/image/slide/slide-img-${event.target.dataset.index}.jpeg`;
+    // console.log(slideImg);
   }
 });
-
-// function () {}
-// () => {}
